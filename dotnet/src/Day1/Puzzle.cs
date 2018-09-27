2b741ead09ebc7147e9f218335625e5c224d8d26
FantasticFiasco <mattias.kindborg@gmail.com>
2018-09-27 07:22:35 +0200

    refactor(dotnet): day 1 part 1

---
+++ dotnet/src/Day1/Puzzle.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCode.Day1
+{
+    public static class Puzzle
+    {
+        public static int SolvePart1(int[] input)
+        {
+            var sum = 0;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var current = input[i];
+                var next = input[(i + 1) % input.Length];
+
+                if (current == next)
+                {
+                    sum += current;
+                }
+            }
+
+            return sum;
+        }
+
+        public static int SolvePart2(int[] input)
+        {
+            var sum = 0;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var current = input[i];
+                var halfwayAround = input[(i + input.Length / 2) % input.Length];
+
+                if (current == halfwayAround)
+                {
+                    sum += current;
+                }
+            }
+
+            return sum;
+        }
+    }
+}

8609ae6bed53b0c3e45e762f9f72cfe005481d4f
FantasticFiasco <mattias.kindborg@gmail.com>
2018-10-03 22:39:47 +0200

    feat(dotnet): day 4

---
+++ dotnet/src/Day4/Puzzle.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace AdventOfCode.Day4
+{
+    public static class Puzzle
+    {
+        public static bool SolvePart1(string passphrase)
+        {
+            var words = passphrase.Split(' ');
+            var distinctWords = words.Distinct().ToArray();
+
+            return words.Length == distinctWords.Length;
+        }
+
+        public static bool SolvePart2(string passphrase)
+        {
+            var words = passphrase
+                .Split(' ')
+                .Select(word => word.ToCharArray())
+                .Select(charArray => charArray.OrderBy(character => character))
+                .Select(charArray => new string(charArray.ToArray()))
+                .ToArray();
+
+            var distinctWords = words.Distinct().ToArray();
+
+            return words.Length == distinctWords.Length;
+        }
+    }
+}
---
+++ dotnet/test/Day4/PuzzleShould.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using System.Linq;
+using Shouldly;
+using Xunit;
+
+namespace AdventOfCode.Day4
+{
+    public class PuzzleShould
+    {
+        [Theory]
+        [InlineData("aa bb cc dd ee", true)]
+        [InlineData("aa bb cc dd aa", false)]
+        [InlineData("aa bb cc dd aaa", true)]
+        public void SolvePart1GivenExamples(string passphrase, bool expectedValidity)
+        {
+            // Act
+            var actualValidity = Puzzle.SolvePart1(passphrase);
+
+            // Assert
+            actualValidity.ShouldBe(expectedValidity);
+        }
+
+        [Fact]
+        public void SolvePart1()
+        {
+            // Arrange
+            var passphrases = File.ReadAllLines(@"Day4\Input.txt");
+
+            // Act
+            var validPassphrases = passphrases
+                .Where(Puzzle.SolvePart1)
+                .ToArray();
+
+            // Assert
+            validPassphrases.Length.ShouldBe(451);
+        }
+
+        [Theory]
+        [InlineData("abcde fghij", true)]
+        [InlineData("abcde xyz ecdab", false)]
+        [InlineData("a ab abc abd abf abj", true)]
+        [InlineData("iiii oiii ooii oooi oooo", true)]
+        [InlineData("oiii ioii iioi iiio", false)]
+        public void SolvePart2GivenExamples(string passphrase, bool expectedValidity)
+        {
+            // Act
+            var actualValidity = Puzzle.SolvePart2(passphrase);
+
+            // Assert
+            actualValidity.ShouldBe(expectedValidity);
+        }
+
+        [Fact]
+        public void SolvePart2()
+        {
+            // Arrange
+            var passphrases = File.ReadAllLines(@"Day4\Input.txt");
+
+            // Act
+            var validPassphrases = passphrases
+                .Where(Puzzle.SolvePart2)
+                .ToArray();
+
+            // Assert
+            validPassphrases.Length.ShouldBe(223);
+        }
+    }
+}
